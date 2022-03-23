e0ecb967489eee969e9cf5ee7c53000513e71504
Caleb Ukle <caleb@nrwl.io>
2022-03-23 15:52:13 -0500

    fix(testing): add root karma config if one isn't present when using karma-project generator
    when using the karma-project generator directory ensure the karma generator (install deps/root
    config) is called if the root karma config isn't present
    
    ISSUES CLOSED: #9234

--- packages/angular/src/generators/karma-project/karma-project.ts
+++ packages/angular/src/generators/karma-project/karma-project.ts
@@ -5,11 +5,15 @@ import { generateKarmaProjectFiles } from './lib/generate-karma-project-files';
 import { updateTsConfigs } from './lib/update-tsconfig';
 import { updateWorkspaceConfig } from './lib/update-workspace-config';
 import type { KarmaProjectOptions } from './schema';
+import { karmaGenerator } from '../karma/karma';
 
 export async function karmaProjectGenerator(
   tree: Tree,
   options: KarmaProjectOptions
 ) {
+  if (!tree.exists('karma.conf.js')) {
+    karmaGenerator(tree, options);
+  }
   checkProjectTestTarget(tree, options.project);
   generateKarmaProjectFiles(tree, options.project);
   updateTsConfigs(tree, options.project);
